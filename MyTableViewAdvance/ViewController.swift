30abf540d5fa6342043cf9bc96cf1a54d9277e2d
saminshakya <saminshakya27@gmail.com>
2023-06-27 11:47:59 +0545

    initial project setup

---
+++ MyTableViewAdvance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTableViewAdvance
+//
+//  Created by MicroBanker Nepal Pvt. Ltd. on 27/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
